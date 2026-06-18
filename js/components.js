fd1b9fc5811f1bc7f6ef5e49997587f46f0c42b0
akshitupadhyay17 <akshitupadhyay1725@gmail.com>
2026-06-18 21:14:25 +0530

    Add Resources section to navigation and footer, including new pages for Blogs, Whitepapers, Patent News, Case Studies, and FAQs

--- js/components.js
+++ js/components.js
@@ -170,6 +170,36 @@ function renderHeader() {
             </div>
           </li>
 
+          <!-- ===== RESOURCES ===== -->
+          <li class="nav__item nav__item--has-dropdown">
+            <a href="#" class="nav__link">
+              Resources
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="6 9 12 15 18 9"/></svg>
+            </a>
+            <div class="nav__dropdown">
+              <a href="${base}resources/blogs.html" class="nav__dropdown-link">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="16" y1="13" x2="8" y2="13"/><line x1="16" y1="17" x2="8" y2="17"/><polyline points="10 9 9 9 8 9"/></svg>
+                Blogs
+              </a>
+              <a href="${base}resources/whitepapers.html" class="nav__dropdown-link">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V8l-6-6z"/><path d="M14 3v5h5M16 13H8M16 17H8M10 9H8"/></svg>
+                Whitepapers
+              </a>
+              <a href="${base}resources/patent-news.html" class="nav__dropdown-link">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"/></svg>
+                Patent News
+              </a>
+              <a href="${base}resources/case-studies.html" class="nav__dropdown-link">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 12h-4l-3 9L9 3l-3 9H2"/></svg>
+                Case Studies
+              </a>
+              <a href="${base}resources/faqs.html" class="nav__dropdown-link">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><line x1="12" y1="17" x2="12.01" y2="17"/></svg>
+                FAQs
+              </a>
+            </div>
+          </li>
+
           <li class="nav__item">
             <a href="${base}index.html#why-us" class="nav__link">About</a>
           </li>
@@ -208,7 +238,7 @@ function renderFooter() {
   footer.innerHTML = `
     <footer class="footer">
       <div class="container">
-        <div class="footer__grid footer__grid--5col">
+        <div class="footer__grid footer__grid--6col">
           <div class="footer__brand">
             <a href="${base}index.html" class="nav__logo">
               <div class="nav__logo-icon">V</div>
@@ -251,6 +281,17 @@ function renderFooter() {
               <a href="${base}digital-marketing/ui-ux-design.html" class="footer__link">UI/UX Design</a>
               <a href="${base}digital-marketing/social-media-marketing.html" class="footer__link">Social Media Marketing</a>
               <a href="${base}digital-marketing/paid-ads.html" class="footer__link">Paid Ads</a>
+            </div>
+          </div>
+
+          <div class="footer__col">
+            <h4 class="footer__heading">Resources</h4>
+            <div class="footer__links">
+              <a href="${base}resources/blogs.html" class="footer__link">Blogs</a>
+              <a href="${base}resources/whitepapers.html" class="footer__link">Whitepapers</a>
+              <a href="${base}resources/patent-news.html" class="footer__link">Patent News</a>
+              <a href="${base}resources/case-studies.html" class="footer__link">Case Studies</a>
+              <a href="${base}resources/faqs.html" class="footer__link">FAQs</a>
             </div>
           </div>
 
