cb7a0378d95f4918dc606406633f4fc256e38896
akshitupadhyay17 <akshitupadhyay1725@gmail.com>
2026-06-23 01:40:03 +0530

    Update branding to VISHARA IP and contact emails to visharaip.com

--- js/components.js
+++ js/components.js
@@ -35,7 +35,7 @@ function renderHeader() {
       <div class="nav__inner">
         <a href="${base}index.html" class="nav__logo" id="navLogo">
           <div class="nav__logo-icon">V</div>
-          <span>VISHARA</span>
+          <span>VISHARA IP</span>
         </a>
 
         <ul class="nav__menu" id="navMenu">
@@ -246,7 +246,7 @@ function renderFooter() {
           <div class="footer__brand">
             <a href="${base}index.html" class="nav__logo">
               <div class="nav__logo-icon">V</div>
-              <span>VISHARA</span>
+              <span>VISHARA IP</span>
             </a>
             <p class="footer__brand-text">
               "Where Innovation Grows." 🌱<br><br>
@@ -302,7 +302,7 @@ function renderFooter() {
           <div class="footer__col">
             <h4 class="footer__heading">Contact</h4>
             <div class="footer__links">
-              <a href="mailto:info@vishara.com" class="footer__link">info@vishara.com</a>
+              <a href="mailto:info@visharaip.com" class="footer__link">info@visharaip.com</a>
               <a href="tel:+1234567890" class="footer__link">+1 (234) 567-890</a>
             </div>
             <h4 class="footer__heading" style="margin-top: var(--space-6)">Company</h4>
@@ -315,7 +315,7 @@ function renderFooter() {
         </div>
 
         <div class="footer__bottom">
-          <p class="footer__copyright">© ${year} VISHARA. All rights reserved.</p>
+          <p class="footer__copyright">© ${year} VISHARA IP. All rights reserved.</p>
           <div class="footer__social">
             <a href="#" class="footer__social-link" aria-label="LinkedIn">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect x="2" y="9" width="4" height="12"/><circle cx="4" cy="4" r="2"/></svg>
